47805880e83bb1238980c6401626094c70f3aafd
dingzhengru <qws7869vdx@gmail.com>
2020-12-22 14:01:46 +0800

    新增 v-deep

--- src/views/Style.vue
+++ src/views/Style.vue
@@ -1,14 +1,19 @@
 <template>
   <h1>Style</h1>
-  <h2>v-slotted</h2>
+  <h2>::v-slotted</h2>
   <p>slot 的內容是屬於父組件的，這時候我們可以使用 ::v-slotted 在組件裡設置 slot 的 style</p>
 
   <AppStyleTest>
     <span>測試</span>
   </AppStyleTest>
 
-  <h2>v-global</h2>
+  <h2>::v-global</h2>
   <p>可以在 style scoped 宣告一個全域的樣式。</p>
+
+  <h2>::v-deep 寫法稍微改變</h2>
+  <p>將樣式穿透至子元素</p>
+
+  <code> ::v-deep { .v-input {} } 改成 ::v-deep(.v-input) {} </code>
 </template>
 
 <script>
